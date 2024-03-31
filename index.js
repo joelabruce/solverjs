678d5ad2676e4ed1dca0c4411ee32e444967cd21
LAPTOP-OEHQIHDL\joela <yoale.bruce@gmail.com>
2024-03-31 17:19:06 -0600

    Include updates to index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+redisClient.set('test', 0);
+
 const express = require('express');
 const app = express();
-
 app.get('/', (req, res) =>{
-    res.send('Hi there');
-})
+    var test = Math.random(); 
+    redisClient.get('test', (err, valueItem) => {
+        res.send(valueItem);
+        redisClient.set('test', parseFloat(test));
+    });
+});
 
 app.listen(8081, () => {
     console.log('Listening on port 8081');
-})
+});
